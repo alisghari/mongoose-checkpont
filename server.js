923ce61165fd040505d1ee2eaca9fc1fea513ff7
anasbenyaiche <anas.benyaiche@yahoo.fr>
2021-01-31 10:18:39 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const userRoute = require('./src/route/user')
+
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+app.options('*', cors());
+
+
+app.use('/user', userRoute);
+
+const PORT = process.env.PORT || 8000;
+
+
+const dbURL = 'mongodb://localhost:27017/profiles'
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
+    if (err) {
+        console.log('Error connecting to: ' + dbURL)
+    }
+    else {
+        console.log('Connected to: ' + dbURL)
+    }
+})
+
+app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+});
